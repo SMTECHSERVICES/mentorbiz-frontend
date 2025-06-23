96c249b154716d86fa082f6cb38503ab562bf5f5
Er.Abhishek Mishra <abhishekmishra59@gmail.com>
2025-06-23 17:54:28 +0530

    Update section

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -16,7 +16,7 @@ const ServiceCard = ({ imageUrl, title, description, buttonText='Explore',   onB
       {/* Content Section */}
       <div className="p-6">
         {/* Title */}
-        <h3 className="text-2xl font-bold text-blue-700 mb-3 leading-tight">
+        <h3 className="text-2xl font-bold text-black mb-3 leading-tight">
           {title}
         </h3>
 
