1d04f60ac874896956833ccf7a4a0ceb46eb7bf6
Er.Abhishek Mishra <abhishekmishra59@gmail.com>
2025-07-18 13:07:36 +0530

    enhacne some functionality

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:5000/api"
+//export const server = "http://localhost:5000/api"
 
-//export const server = import.meta.env.VITE_BACKEND_URL
+export const server = import.meta.env.VITE_BACKEND_URL
